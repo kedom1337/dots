986e2744b20719afa6e82485a0e474350fb43325
kedom1337 <kedom@vmcall.net>
2024-04-17 01:14:16 +0200

    fix(nvim): cmp snippet jumps

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -32,18 +32,14 @@ return {
           select = true,
         }),
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.locally_jumpable(1) then
+          if luasnip.locally_jumpable(1) then
             luasnip.jump(1)
           else
             fallback()
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
+          if luasnip.locally_jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
