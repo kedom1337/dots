3b20cac41fffdbde8f76af09d7e6a5ae63d4f8dd
kedom1337 <kedom@vmcall.net>
2024-04-17 00:18:20 +0200

    feat(fish): migrated to abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,21 +4,19 @@ if status is-interactive
 
     fish_config theme choose "Catppuccin Mocha"
 
-    # Vi keybindings
     fish_vi_key_bindings
-    source ~/.config/fish/functions/fish_mode_promt.fish
+
+    abbr -a !! --position anywhere --function last_history_item
 
     # Pacman and AUR package managment
-    alias pacin="pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
-    alias pacrm="pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns"
-    alias yayin="yay -Slq | fzf --multi --preview 'yay -Si {1}' | xargs -ro yay -S"
-    alias yayrm="yay -Qq | fzf --multi --preview 'yay -Qi {1}' | xargs -ro yay -Rns"
-
+    abbr -a pacin "pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
+    abbr -a pacrm "pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns"
+    abbr -a yayin "yay -Slq | fzf --multi --preview 'yay -Si {1}' | xargs -ro yay -S"
+    abbr -a yayrm "yay -Qq | fzf --multi --preview 'yay -Qi {1}' | xargs -ro yay -Rns"
 
     # Applications
-    alias y="yazi"
-    alias lgit="lazygit"
-    alias ls="eza -al --color=always --group-directories-first"
-    alias lt="eza -aT --color=always --group-directories-first"
-
+    abbr -a y "yazi"
+    abbr -a lgit "lazygit"
+    abbr -a ls "eza -al --color=always --group-directories-first"
+    abbr -a lt "eza -aT --color=always --group-directories-first"
 end
