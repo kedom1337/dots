12721f72d8e5ef2dfea58573430bf822d3d714b4
kedom1337 <kedom@vmcall.net>
2024-08-25 23:33:40 +0200

    feat(neovim): switched toggleterm with lazygit

---
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,16 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "<a-i>", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+  }
+}
